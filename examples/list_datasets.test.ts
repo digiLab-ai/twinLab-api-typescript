2c8023709a8b803e44a34fae3d13c3b095fee784
FreddyWordingham <freddy@digilab.co.uk>
2023-06-21 22:10:10 +0100

    Improved test example

--- examples/list_datasets.test.ts
+++ examples/list_datasets.test.ts
@@ -1,8 +1,7 @@
 import { listDatasets } from "../api";
 
 test("List Datasets", () => {
-    listDatasets().then((response) => {
-        // console.log(JSON.stringify(response, null, 4));
-        expect(response).toStrictEqual([]);
+    return listDatasets().then((response) => {
+        console.log(JSON.stringify(response, null, 4));
     });
 });
